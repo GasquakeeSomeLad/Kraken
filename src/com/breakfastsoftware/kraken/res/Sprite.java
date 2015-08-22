ed23554d694feb51464e5e2befcadd73f12e90af
GasquakeeSomeLad <GasquakeeSomeLad@gmail.com>
2015-08-22 03:49:10 -1100

    Edited

--- src/com/breakfastsoftware/kraken/res/Sprite.java
+++ src/com/breakfastsoftware/kraken/res/Sprite.java
@@ -4,9 +4,11 @@ package com.breakfastsoftware.kraken.res;
  * Created by SomeLad on 8/21/2015.
  */
 public class Sprite {
-    public final int[] PIXELS;
+    public final int WIDTH, HEIGHT, PIXELS[];
 
     public Sprite(SpriteSheet sheet, int x, int y, int w, int h) {
         PIXELS = sheet.getPixels(x, y, w, h);
+        WIDTH = w;
+        HEIGHT = h;
     }
 }
