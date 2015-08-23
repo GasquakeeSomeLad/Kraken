f21c41d3bca99d505d1776d074f7a92ad4a36d7a
GasquakeeSomeLad <GasquakeeSomeLad@gmail.com>
2015-08-23 07:50:41 -1100

    Edited

--- src/com/breakfastsoftware/kraken/res/audio/Sound.java
+++ src/com/breakfastsoftware/kraken/res/audio/Sound.java
@@ -16,7 +16,6 @@ public enum Sound {
     private static Random generator = new Random();
 
     private int amount;
-    private String name;
     private Clip[] clips;
 
     private Sound(String name, int amount) {
@@ -24,13 +23,11 @@ public enum Sound {
             System.out.println("\nSounds Loading...");
         }
 
-        this.name = name;
         this.amount = amount;
         clips = new Clip[amount];
 
         try {
             for (int i = 1; i <= amount; i++) {
-                System.out.println(name+i+".wav loaded");
                 AudioInputStream stream = AudioSystem.getAudioInputStream(Sound.class.getResource(name + i + ".wav"));
                 clips[i-1] = AudioSystem.getClip();
                 clips[i-1].open(stream);
